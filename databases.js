8979d239494dffac5399701e51d2743bd1817eee
Ayagari Mukund <ayagarimukund@Ayagaris-MacBook-Air.local>
2023-07-31 17:38:39 +0800

    gave up and switched back to mysql

--- databases.js
+++ databases.js
@@ -1,28 +1,35 @@
 //get environment variables to work before commiting to github
 //import mysql from 'mysql2'
-//const mysql = require('mysql2/promise')
-const {Pool} = require('pg')
+const mysql = require('mysql2/promise')
+//const {Pool} = require('pg')
 if(process.env !== 'production'){
     require('dotenv').config()
 }
 
-/*
+
 const pool = mysql.createPool({
   
 
+    /*host: process.env.host,
+    username: process.env.username,
+    password: process.env.password,
+    database: process.env.database 
+    */
+    
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE 
+    
 })
-*/
 
-const pool = new Pool({
+
+/*const pool = new Pool({
     host: process.env.host,
     username: process.env.username,
     password: process.env.password,
     database: process.env.database 
-})
+})*/
 
 module.exports = pool
 
