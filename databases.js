a241333ede677cb53270be48944e375c9c91b38f
Ayagari Mukund <ayagarimukund@Ayagaris-MacBook-Air.local>
2023-08-02 10:45:48 +0800

    this is the one

--- databases.js
+++ databases.js
@@ -19,7 +19,10 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE 
+    database: process.env.MYSQL_DATABASE, 
+    ssl:{
+        rejectUnauthorized: true
+    }
     
 })
 
